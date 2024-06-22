559bed942eef984e81baacfd5e3eee3dea4bad57
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-06-22 20:50:26 +0300

    feat: add HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./HomePage.module.scss";
+
 export default function HomePage() {
-  return <h1>It's home </h1>;
+  return (
+    <div className={styles.homePage}>
+      <h1 className={styles.homePage__title}>LEARNING</h1>
+      <p className={styles.homePage__about}>
+        Language Learning Cards is a study tool that makes learning and
+        memorizing information a fun, stress-free, and enjoyable experience.
+      </p>
+      <p className={styles.homePage__toLogin}>
+        Please log in to proceed with your studies.
+      </p>
+      <Link className={styles.homePage__linkLogin} to="/login">
+        {" "}
+        Go to Login Page
+      </Link>
+    </div>
+  );
 }
