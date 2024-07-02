d10806f648f5f4f5a994e32f0caa8646546f2738
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-07-02 15:40:22 +0300

    Revert "feat: add open replay code"
    This reverts commit 41597e45d69ba085df5b4be269b7ebd5c44c05db.
    
    fix: revert adding open replay code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import styles from "./commonStyles/index.module.scss";
 import App from "./App";
-import Tracker from "@openreplay/tracker";
-
-const tracker = new Tracker({
-  projectKey: "I7qni2iDORi17F7iJhWT",
-});
-tracker.start();
-tracker.setUserID("Elena");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
